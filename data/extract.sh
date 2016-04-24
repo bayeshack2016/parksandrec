610c5b50deec4daeea55417ac3c677c3948defcd
juanshishido <juanshishido@cal.berkeley.edu>
2016-04-24 01:13:42 -0700

    RIDB data

--- data/extract.sh
+++ data/extract.sh
@@ -1,8 +1,13 @@
 mkdir -p raw/yelp
+mkdir raw/ridb
 mkdir clean
 
 for f in *.zip; do
-    unzip $f -d raw/;
+    if [[ $f = "RIDBFullExport_v1.zip" ]]; then
+        unzip $f -d raw/ridb
+    else
+        unzip $f -d raw/
+    fi
 done
 
 tar -xf yelp_dataset_challenge_academic_dataset.tar --directory raw/yelp
